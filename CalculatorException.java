a05e6de2fdc610424143192e453ebf8ed9eb98eb
Gabriel <electengrhume@gmail.com>
2015-06-11 17:44:59 +0100

    CalculatorException.java

---
+++ CalculatorException.java
@@ -0,0 +1,12 @@
+package mycalculator;
+
+/**
+ * Created by Computation on 6/6/15.
+ */
+public class CalculatorException extends Exception
+{
+    public CalculatorException()
+    {
+        super("No value entered.");
+    }
+}
